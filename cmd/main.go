164004ec2a40511885e1a6007b3cc3cf69495bde
alekssro <alekss.ro@gmail.com>
2021-04-18 20:55:59 +0000

    doc: Add granularity options to help

--- cmd/main.go
+++ cmd/main.go
@@ -12,7 +12,7 @@ func main() {
 	p := flag.String("product", "BTC-EUR", "Crypto trade value to store")
 	f := flag.String("filename", "dataset.csv", "Filename to save the dataset to")
 	t := flag.Int("time", 30, "Time of data collection, in days")
-	g := flag.Int("granularity", 21600, "Time between data points, in seconds")
+	g := flag.Int("granularity", 21600, "Time between data points, in seconds. Options: {60, 300, 900, 3600, 21600, 86400}.")
 	flag.Parse()
 
 	logger.Info("Coinbase history collector started...")
